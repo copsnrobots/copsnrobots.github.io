afc6f689ce4fa3da63ec254a13efe2e7f6549c61
copsnrobots <43809189+copsnrobots@users.noreply.github.com>
2018-09-29 10:19:46 +0200

    Extract variable number of suspect cards

--- src/extract.sh
+++ src/extract.sh
@@ -1,15 +1,20 @@
 set -eu -o pipefail
 
 extract_set() {
-    backpage=$[start + 1]
-    mkdir -p images/$cardset/
-    convert -density 300 "$file[$((start+1))]" -crop $cardsize+150+150 -rotate 90 "images/$cardset/back.png"
-    convert -density 300 "$file[$start]" -crop $cardsize+150+150 -rotate 90 "images/$cardset/human.png"
+    backpage=$((start + 1))
+    mkdir -p images/$cardset/{suspect,investigator}
+    convert -density 300 "$file[$((start+1))]" -crop $cardsize+150+150 -rotate 90 "images/$cardset/suspect/back.png"
+    convert -density 300 "$file[$start]" -crop $cardsize+150+150 -rotate 90 "images/$cardset/suspect/human.png"
     i=0
-    for page in $((start+4)) $((start+6)); do
+    page=$((start+4))
+    while true; do
         for offset in 150 1237 2325; do
-            convert -density 300 "$file[$page]" -crop $cardsize+150+$offset -rotate 90 "images/$cardset/robot-$((i++)).png"
+            convert -density 300 "$file[$page]" -crop $cardsize+150+$offset -rotate 90 "images/$cardset/suspect/robot-$((i++)).png"
+            if [ $i -eq $count ]; then
+                break 2
+            fi
         done
+        page=$((page+2))
     done
 }
 
@@ -18,4 +23,10 @@ cardsize=2100x825
 
 cardset=chair
 start=2
+count=6
+extract_set
+
+cardset=jug
+start=26
+count=7
 extract_set
