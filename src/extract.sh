8ba1b3993c1cd956d63405fddd15811a354ab90a
copsnrobots <43809189+copsnrobots@users.noreply.github.com>
2018-09-29 10:58:37 +0200

    Extract prompts

--- src/extract.sh
+++ src/extract.sh
@@ -13,7 +13,7 @@ extract_faces() {
                 local suffix="-$i.png"
             fi
 
-            convert -density 300 "$file[$page]" -crop $cardsize+150+$offset -rotate 90 "$path$suffix"
+            convert -density 300 "$file[$page]" -crop $cardsize+150+$offset -rotate "$rotation" "$path$suffix"
             i=$((i+1))
             if [ "$i" -eq "$count" ]; then
                 break 2
@@ -25,9 +25,14 @@ extract_faces() {
 
 extract_set() {
     mkdir -p images/$cardset/{suspect,investigator}
+    rotation="90"
     extract_faces "$((start+1))" "images/$cardset/suspect/back" 1
     extract_faces "$start" "images/$cardset/suspect/human" 1
     extract_faces "$((start+4))" "images/$cardset/suspect/robot" $count
+    rotation="0"
+    extract_faces "$((questionstart+1))" "images/$cardset/investigator/back" 1
+    extract_faces "$((questionstart))" "images/$cardset/investigator/secondary" 3
+    extract_faces "$((questionstart+2))" "images/$cardset/investigator/primary" 3
 }
 
 file="pdfs/Inhuman Conditions Print & Play (Updated Public File).pdf"
@@ -36,9 +41,11 @@ cardsize=2100x825
 cardset=chair
 start=2
 count=6
+questionstart=36
 extract_set
 
 cardset=jug
 start=26
 count=7
+questionstart=48
 extract_set
