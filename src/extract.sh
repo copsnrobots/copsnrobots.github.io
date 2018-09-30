37818a145435f1d338bbc624738c545a162ab1cf
copsnrobots <43809189+copsnrobots@users.noreply.github.com>
2018-09-30 12:55:50 +0200

    Output to cards/ and add cards/sets.json

--- src/extract.sh
+++ src/extract.sh
@@ -9,7 +9,7 @@ extract() {
     local outfile=$3
     if ! [ -f "$outfile" ]; then
         if ! convert -density 300 "$file[$page]" -crop "$crop" -rotate "$rotation" "$outfile"; then
-            echo 'Failed command:' convert -density 300 "'$file[$page]'" -crop "$crop" -rotate "$rotation" "'$outfile'"
+            echo 'Failed command:' convert -density 300 "'$file[$page]'" -crop "$crop" -rotate "$rotation" "'$outfile'" >&2
             exit
         fi
     fi
@@ -41,38 +41,50 @@ extract_faces() {
 }
 
 extract_set() {
-    mkdir -p images/$cardset/{suspect,investigator}
+    mkdir -p cards/$cardset/{suspect,investigator}
+    echo -e '\t"'"$cardset"'": {'
+    echo -e '\t\t"description": '"$setname"'",'
+    echo -e '\t\t"humans": 6,'
+    echo -e '\t\t"robots": '"$count"
+    echo -e '\t}'
     rotation="90"
-    extract_faces "$((start+1))" "images/$cardset/suspect/back" 1
-    extract_faces "$start" "images/$cardset/suspect/human" 1
-    extract_faces "$((start+4))" "images/$cardset/suspect/robot" $count
+    extract_faces "$((start+1))" "cards/$cardset/suspect/back" 1
+    extract_faces "$start" "cards/$cardset/suspect/human" 1
+    extract_faces "$((start+4))" "cards/$cardset/suspect/robot" $count
     rotation="0"
-    extract_faces "$((questionstart+1))" "images/$cardset/investigator/back" 1
-    extract_faces "$((questionstart))" "images/$cardset/investigator/secondary" 3
-    extract_faces "$((questionstart+2))" "images/$cardset/investigator/primary" 3
+    extract_faces "$((questionstart+1))" "cards/$cardset/investigator/back" 1
+    extract_faces "$((questionstart))" "cards/$cardset/investigator/secondary" 3
+    extract_faces "$((questionstart+2))" "cards/$cardset/investigator/primary" 3
 }
 
 file="pdfs/Inhuman Conditions Print & Play (Updated Public File).pdf"
 
+exec 1>"cards/sets.json"
+echo '{'
+trap "echo '}'" EXIT
 cardset=chair
+setname="Process Your Day"
 start=2
 count=6
 questionstart=36
 extract_set
 
 cardset=dragon
+setname="Demonstrate Imagination"
 start=10
 count=6
 questionstart=40
 extract_set
 
 cardset=rose
+setname="Experience and Process Grief"
 start=18
 count=6
 questionstart=44
 extract_set
 
 cardset=coffeepot
+setname="Apply Creative Problem Solving"
 start=26
 count=7
 questionstart=48
@@ -82,15 +94,15 @@ rotation=0
 
 cardsize=806x$triple_height
 page=62
-mkdir -p "images/notes/"
-extract "$((page+1))" "$cardsize+150+150" "images/notes/back.png"
+mkdir -p "cards/notes/"
+extract "$((page+1))" "$cardsize+150+150" "cards/notes/back.png"
 
 i=0
 count=26
 while true; do
     for offset_y in $triple_split; do
         for offset_x in 150 1592; do
-            extract "$page" "$cardsize+$offset_x+$offset_y" "images/notes/$i.png"
+            extract "$page" "$cardsize+$offset_x+$offset_y" "cards/notes/$i.png"
             i=$((i+1))
             if [ "$i" -eq "$count" ]; then
                 break 3
@@ -102,15 +114,15 @@ done
 
 cardsize=825x1275
 page=52
-mkdir -p "images/penalties/"
-extract "$((page+1))" "$cardsize+150+150" "images/penalties/back.png"
+mkdir -p "cards/penalties/"
+extract "$((page+1))" "$cardsize+150+150" "cards/penalties/back.png"
 
 i=0
 count=17
 while true; do
     for offset_y in 150 1873; do
         for offset_x in 150 1573; do
-            extract "$page" "$cardsize+$offset_x+$offset_y" "images/penalties/$i.png"
+            extract "$page" "$cardsize+$offset_x+$offset_y" "cards/penalties/$i.png"
             i=$((i+1))
             if [ "$i" -eq "$count" ]; then
                 break 3
