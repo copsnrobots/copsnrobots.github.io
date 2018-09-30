e986506cc65fc2cb754416a2586f8e836f6a43d4
copsnrobots <43809189+copsnrobots@users.noreply.github.com>
2018-09-30 14:32:23 +0200

    Rename set/cardset -> packet

--- src/extract.sh
+++ src/extract.sh
@@ -40,61 +40,61 @@ extract_faces() {
     done
 }
 
-extract_set() {
-    mkdir -p cards/$cardset/{suspect,investigator}
-    echo -e '\t"'"$cardset"'": {'
-    echo -e '\t\t"description": "'"$setname"'",'
+extract_packet() {
+    mkdir -p cards/$packet/{suspect,investigator}
+    echo -e '\t"'"$packet"'": {'
+    echo -e '\t\t"description": "'"$packetname"'",'
     echo -e '\t\t"humans": 6,'
     echo -e '\t\t"robots": '"$count"
     echo -en '\t}'
 
     rotation="90"
-    extract "$((start+1))" "200x200+1769+464" "cards/$cardset/icon.png"
-    extract_faces "$((start+1))" "cards/$cardset/suspect/back" 1
-    extract_faces "$start" "cards/$cardset/suspect/human" 1
-    extract_faces "$((start+4))" "cards/$cardset/suspect/robot" $count
+    extract "$((start+1))" "200x200+1769+464" "cards/$packet/icon.png"
+    extract_faces "$((start+1))" "cards/$packet/suspect/back" 1
+    extract_faces "$start" "cards/$packet/suspect/human" 1
+    extract_faces "$((start+4))" "cards/$packet/suspect/robot" $count
 
     rotation="0"
-    extract_faces "$((questionstart+1))" "cards/$cardset/investigator/back" 1
-    extract_faces "$((questionstart))" "cards/$cardset/investigator/secondary" 3
-    extract_faces "$((questionstart+2))" "cards/$cardset/investigator/primary" 3
+    extract_faces "$((questionstart+1))" "cards/$packet/investigator/back" 1
+    extract_faces "$((questionstart))" "cards/$packet/investigator/secondary" 3
+    extract_faces "$((questionstart+2))" "cards/$packet/investigator/primary" 3
 }
 
 file="pdfs/Inhuman Conditions Print & Play (Updated Public File).pdf"
 
-exec 1>"cards/sets.json"
+exec 1>"cards/packets.json"
 echo '{'
 trap "echo '}'" EXIT
-cardset=chair
-setname="Process Your Day"
+packet=chair
+packetname="Process Your Day"
 start=2
 count=6
 questionstart=36
-extract_set
+extract_packet
 echo ','
 
-cardset=dragon
-setname="Demonstrate Imagination"
+packet=dragon
+packetname="Demonstrate Imagination"
 start=10
 count=6
 questionstart=40
-extract_set
+extract_packet
 echo ','
 
-cardset=rose
-setname="Experience and Process Grief"
+packet=rose
+packetname="Experience and Process Grief"
 start=18
 count=6
 questionstart=44
-extract_set
+extract_packet
 echo ','
 
-cardset=coffeepot
-setname="Apply Creative Problem Solving"
+packet=coffeepot
+packetname="Apply Creative Problem Solving"
 start=26
 count=7
 questionstart=48
-extract_set
+extract_packet
 echo ','
 
 rotation=0
@@ -141,9 +141,9 @@ done
 
 file="pdfs/Hope And Dreams Cards (Updated Public File).pdf"
 
-cardset=hourglass
-setname="Hopes and Dreams"
+packet=hourglass
+packetname="Hopes and Dreams"
 start=0
 count=6
 questionstart=8
-extract_set
+extract_packet
